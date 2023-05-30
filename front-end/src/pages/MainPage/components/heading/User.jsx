1fb2356d357f5427d35c599096b2164f79646da2
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-05-30 19:30:13 -0300

    refresh button
    using 'useState' to set the animation on/off on refresh button

--- front-end/src/pages/MainPage/components/heading/User.jsx
+++ front-end/src/pages/MainPage/components/heading/User.jsx
@@ -1,17 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import SyncIcon from "@mui/icons-material/Sync";
 import LoginIcon from "@mui/icons-material/Login";
 import { AuthContext } from "../../../../context/auth.jsx";
 
 function User({ onPress }) {
   const { logout } = useContext(AuthContext);
+  const [rotation, setRotation] = useState(false);
+
   const handleLogout = () => {
     logout();
   };
 
+  const handleRefresh = () => {
+    setRotation(true);
+    setTimeout(() => {
+      onPress();
+      setRotation(false);
+    }, 1000);
+  };
+
   return (
     <div className="container heading-padding">
-      <button className="rounded-btn" title="Refresh" onClick={onPress}>
+      <button
+        className={rotation ? "rounded-btn image" : "rounded-btn"}
+        title="Refresh"
+        onClick={handleRefresh}
+      >
         <SyncIcon />
       </button>
       <button className="rounded-btn" title="Login" onClick={handleLogout}>
