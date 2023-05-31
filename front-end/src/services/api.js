b6bc44f5a5aa6a6175666e61d82875779e421977
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-05-31 01:36:11 -0300

    Update api.js
    type error fixed ${repositoryId} > ${noteId}

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -4,29 +4,28 @@ export const api = axios.create({
   baseURL: "http://localhost:5000/",
 });
 
-
 export const createUser = async (email, password) => {
   return api.post("/users/", { email, password });
 };
-
 
 export const createSession = async (email, password) => {
   return api.post("/sessions/", { email, password });
 };
 
-
 export const createNote = async (userId, title, content, status) => {
   return api.post(`/notes/${userId}/`, { title, content, status });
 };
-
 
 export const getNotes = async (userId) => {
   return api.get(`/notes/${userId}/`);
 };
 
+export const getOneNote = async (userId, noteId) => {
+  return api.get(`/notes/${userId}/${noteId}/`);
+};
 
-export const destroyRepository = async (userId, repositoryId) => {
-  const url = `/users/repositories/${userId}/${repositoryId}/`;
+export const destroyNote = async (userId, noteId) => {
+  const url = `/notes/${userId}/${noteId}/`;
 
   return api.delete(url);
 };
