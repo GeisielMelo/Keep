e4466ea8c52a7e0d7d521898d3291906e78dd8d3
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-05-31 19:56:35 -0300

    Update Input.jsx
    value={props.value} added, indentation fix

--- front-end/src/pages/LoginPage/components/Input.jsx
+++ front-end/src/pages/LoginPage/components/Input.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 function Input(props) {
   return (
-      <input
-        className="input-box shadow"
-        type={props.type}
-        placeholder={props.placeholder}
-        name={props.name}
-        onChange={props.onChange}
-      />
+    <input
+      className="input-box shadow"
+      type={props.type}
+      placeholder={props.placeholder}
+      name={props.name}
+      value={props.value}
+      onChange={props.onChange}
+    />
   );
 }
 
