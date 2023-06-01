b543c81bb04a9fb679ff880b8dac68bbace6a009
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-06-01 17:46:48 -0300

    Importing disableReactDevTools

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+if (process.env.NODE_ENV === "production") disableReactDevTools();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
