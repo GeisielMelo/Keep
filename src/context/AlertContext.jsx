ba12173aefb1d34504cc88e66d95314366f7b59d
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-16 12:26:29 -0300

    refactor: Renamed useAlert and moved it to the context folder.

--- src/context/AlertContext.jsx
+++ src/context/AlertContext.jsx
@@ -46,24 +46,6 @@ export const AlertProvider = ({ children }) => {
   )
 }
 
-// This is a hook for displaying user feedback messages.
-
-// To use it, wrap your App.jsx component with the AlertProvider.
-
-// Import AlertProvider at the beginning of your file:
-// import { AlertProvider } from './hook/useAlert'
-
-// Wrap your App component with the AlertProvider:
-// <AlertProvider>
-//    <App />
-// </AlertProvider>
-
-// Inside your component, import the useAlert hook and use it like this:
-// const { showAlert } = useAlert();
-
-// Now, you can use the showAlert function to display messages to the user:
-// showAlert('This is a success message', 'success');
-
 // Reference
 // https://medium.com/@alokjain.dev/creating-custom-alert-notifications-in-react-with-material-ui-snackbar-73effbedbd40
 // https://stackoverflow.com/questions/72640471/cant-make-this-message-hook-work-in-react
