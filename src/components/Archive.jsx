c236ee2bb40e7c0a142b0c9fb28de2a80f7a7717
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-17 11:01:50 -0300

    feat: Completed archive page with functions for restoration and deletion.

---
+++ src/components/Archive.jsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react'
+import { NotesContext } from '../context/NotesContext'
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import UnarchiveIcon from '@mui/icons-material/Unarchive'
+import { Container } from '../styles/StyledNote'
+
+export const Archive = ({ id, title, description, labels }) => {
+  const { updateNote, removeNote } = useContext(NotesContext)
+
+  const handleUnArchiveNote = async () => {
+    await updateNote(id, title, description, labels, false)
+  }
+
+  return (
+    <Container id={id}>
+      <h1>
+        {title} {id}
+      </h1>
+      <p>{description}</p>
+      <ul>
+        {labels.map((label, index) => (
+          <li key={index}>{label}</li>
+        ))}
+      </ul>
+
+      <div className='buttons'>
+        <button onClick={() => removeNote(id)}>
+          <DeleteForeverIcon style={{ fontSize: '18px' }} />
+        </button>
+        <button onClick={() => handleUnArchiveNote()}>
+          <UnarchiveIcon style={{ fontSize: '18px' }} />
+        </button>
+      </div>
+    </Container>
+  )
+}
--- src/pages/navigation/Archived.jsx
+++ src/pages/navigation/Archived.jsx
@@ -1,4 +1,7 @@
-import { Note } from '../../components/Note'
+import React, { useContext } from 'react'
+import { NotesContext } from '../../context/NotesContext'
+import { Archive } from '../../components/Archive'
+
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -7,19 +10,25 @@ const Container = styled.div`
   gap: 10px;
   max-height: calc(100vh - 70px);
   overflow: auto;
-`;
+`
 
-const Archived = ({ data }) => {
+const Archived = () => {
+  const { notes } = useContext(NotesContext)
   return (
     <Container>
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
-      <Note />
+      {notes.map(
+        (note, index) =>
+          note.archived && (
+            <Archive
+              key={index}
+              id={index}
+              title={note.title}
+              description={note.description}
+              labels={note.labels}
+              archived={note.archived}
+            />
+          ),
+      )}
     </Container>
   )
 }
