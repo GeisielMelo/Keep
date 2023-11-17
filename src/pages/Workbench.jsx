23aeff047e7fed909546210426d30990a415b8b3
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-17 13:30:18 -0300

    refactor: Moved NotesProvider to AppRoutes.

--- src/pages/Workbench.jsx
+++ src/pages/Workbench.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { NotesProvider } from '../context/NotesContext'
 import { Nav, Section, Menu, Content, Container } from '../styles/StyledWorkbench'
 import { MenuButton } from '../components/MenuButton'
 
@@ -32,59 +31,61 @@ const Workbench = ({ children }) => {
     setTimeout(() => setIsDisabled(false), 500)
   }
 
+  const handleButtonClick = (page) => {
+    navigate(page)
+  }
+
   return (
-    <NotesProvider>
-      <Section>
-        <Nav>
-          <div className='left-container'>
-            <button disabled={isDisabled} onClick={() => handleMenuOpen()}>
-              {isOpen ? <MenuOpenIcon /> : <MenuIcon />}
-            </button>
-            <div>
-              <img src='./svg/logo.svg' alt='' />
-              <h1>Reminders</h1>
-            </div>
+    <Section>
+      <Nav>
+        <div className='left-container'>
+          <button disabled={isDisabled} onClick={() => handleMenuOpen()}>
+            {isOpen ? <MenuOpenIcon /> : <MenuIcon />}
+          </button>
+          <div>
+            <img src='./svg/logo.svg' alt='' />
+            <h1>Reminders</h1>
           </div>
+        </div>
 
-          <div className='middle-container'>
-            <input type='text' placeholder='Search' />
-          </div>
+        <div className='middle-container'>
+          <input type='text' placeholder='Search' />
+        </div>
 
-          <div className='right-container'>
-            <button>
-              <CloudIcon />
-            </button>
-            <button>
-              <PersonIcon />
-            </button>
-          </div>
-        </Nav>
+        <div className='right-container'>
+          <button>
+            <CloudIcon />
+          </button>
+          <button>
+            <PersonIcon />
+          </button>
+        </div>
+      </Nav>
 
-        <Container>
-          <Menu data-open={isOpen}>
-            <MenuButton
-              onClick={() => navigate('/home')}
-              isTextVisible={isTextVisible}
-              text={'Notes'}
-              icon={<TipsAndUpdatesIcon />}
-            />
-            <MenuButton
-              onClick={() => navigate('/labels')}
-              isTextVisible={isTextVisible}
-              text={'Labels'}
-              icon={<LabelIcon />}
-            />
-            <MenuButton
-              onClick={() => navigate('/archive')}
-              isTextVisible={isTextVisible}
-              text={'Archive'}
-              icon={<UnarchiveIcon />}
-            />
-          </Menu>
-          <Content>{children}</Content>
-        </Container>
-      </Section>
-    </NotesProvider>
+      <Container>
+        <Menu data-open={isOpen}>
+          <MenuButton
+            onClick={() => handleButtonClick('/home')}
+            isTextVisible={isTextVisible}
+            text={'Notes'}
+            icon={<TipsAndUpdatesIcon />}
+          />
+          <MenuButton
+            onClick={() => handleButtonClick('/labels')}
+            isTextVisible={isTextVisible}
+            text={'Labels'}
+            icon={<LabelIcon />}
+          />
+          <MenuButton
+            onClick={() => handleButtonClick('/archive')}
+            isTextVisible={isTextVisible}
+            text={'Archive'}
+            icon={<UnarchiveIcon />}
+          />
+        </Menu>
+        <Content>{children}</Content>
+      </Container>
+    </Section>
   )
 }
 
