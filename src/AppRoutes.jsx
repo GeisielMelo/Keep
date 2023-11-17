28fb4e01540c2501fe403c0656a647cca19ed7b8
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-17 10:58:55 -0300

    feat: Wrapped private routes with the NotesProvider.

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { AuthProvider, AuthContext } from './context/AuthContext'
 import { AlertProvider } from './context/AlertContext'
+import { NotesProvider } from './context/NotesContext'
 
 import Index from './pages/Index'
 import Login from './pages/Login'
@@ -15,12 +16,24 @@ import Labels from './pages/navigation/Labels'
 const AppRoutes = () => {
   const Private = ({ children }) => {
     const { authenticated, loading } = useContext(AuthContext)
-    return loading ? <span>Loading...</span> : authenticated ? children : <Navigate to='/sign-in' />
+    return loading ? (
+      <span>Loading...</span>
+    ) : authenticated ? (
+      children
+    ) : (
+      <Navigate to='/sign-in' />
+    )
   }
 
   const Public = ({ children }) => {
     const { authenticated, loading } = useContext(AuthContext)
-    return loading ? <span>Loading...</span> : authenticated ? <Navigate to='/' /> : children
+    return loading ? (
+      <span>Loading...</span>
+    ) : authenticated ? (
+      <Navigate to='/' />
+    ) : (
+      children
+    )
   }
 
   return (
@@ -31,12 +44,59 @@ const AppRoutes = () => {
             <Route path='/' element={<Index />} />
             <Route path='*' element={<Login />} />
 
-            <Route path='/sign-in' element={<Public><Login /></Public>}/>
-            <Route path='/sign-up' element={<Public><Register /></Public>}/>
+            <Route
+              path='/sign-in'
+              element={
+                <Public>
+                  <Login />
+                </Public>
+              }
+            />
+            <Route
+              path='/sign-up'
+              element={
+                <Public>
+                  <Register />
+                </Public>
+              }
+            />
 
-            <Route path='/home' element={<Private><Workbench><Notes /></Workbench></Private>}/>
-            <Route path='/labels' element={<Private><Workbench><Labels /></Workbench></Private>}/>
-            <Route path='/archive' element={<Private><Workbench><Archived /></Workbench></Private>}/>
+            <Route
+              path='/home'
+              element={
+                <Private>
+                  <NotesProvider>
+                    <Workbench>
+                      <Notes />
+                    </Workbench>
+                  </NotesProvider>
+                </Private>
+              }
+            />
+            <Route
+              path='/labels'
+              element={
+                <Private>
+                  <NotesProvider>
+                    <Workbench>
+                      <Labels />
+                    </Workbench>
+                  </NotesProvider>
+                </Private>
+              }
+            />
+            <Route
+              path='/archive'
+              element={
+                <Private>
+                  <NotesProvider>
+                    <Workbench>
+                      <Archived />
+                    </Workbench>
+                  </NotesProvider>
+                </Private>
+              }
+            />
           </Routes>
         </AuthProvider>
       </AlertProvider>
