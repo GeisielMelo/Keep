a04f400d11fde8b88bbcbe76cf6678204d285760
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-20 14:16:12 -0300

    raw: updated keys value

--- src/styles/theme.jsx
+++ src/styles/theme.jsx
@@ -1,29 +1,23 @@
 export const theme = {
   color: {
-    black: {
-      default: "#000000",
-      light: "#393E46",
-    },
-    white: {
-      default: "#ededed",
-      light: "#EEEEEE",
-    },
+    one: 'rgba(32,33,36,255)',
+    two: 'rgba(255,255,255,255)',
+    three: 'rgba(82,83,85,255)',
+    four: 'rgba(89,92,97,255)',
+    five: 'rgba(65,51,28,255)',
+    six: 'rgba(40,42,44,255)',
+    seven: 'rgb(181,182,182)',
+    transparent: 'rgb(0, 0, 0, 0.3)',
   },
   font: {
     family: {
       one: "'Noto Sans', sans-serif",
     },
     size: {
-      bg: "32px",
-      md: "24px",
-      sm: "14px",
-      es: "10px",
-    },
-    weight: {
-      light: 300,
-      regular: 500,
-      bold: 700,
-      black: 900,
+      bg: '32px',
+      md: '24px',
+      sm: '16px',
+      es: '14px',
     },
   },
-};
+}
