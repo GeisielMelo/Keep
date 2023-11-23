745b4d10171e1a93f8780b5701ae017d4bfaeb5a
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-23 13:20:19 -0300

    refactor: Refactored Loading.jsx animated component for improved usability and readability.

--- src/components/animated/Loading.jsx
+++ src/components/animated/Loading.jsx
@@ -1,37 +1,37 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  width: ${(props) => props.maxWidth || "25px"};
-  height: ${(props) => props.maxHeight || "25px"};
-  border-radius: 50%;
-`;
+import styled from 'styled-components'
 
 const Spinner = styled.div`
-  border: 4px solid #222831;
-  border-top: 4px solid #393E46;
+  width: ${(props) => props.size || '25px'};
+  height: ${(props) => props.size || '25px'};
   border-radius: 50%;
-  border-style: dotted;
-  max-width: ${(props) => props.maxWidth || "25px"};
-  max-height: ${(props) => props.maxHeight || "25px"};
-  width: 100%;
-  height: 100%;
-  
-  animation: spin 2s linear infinite;
-  transition: all 0.2s ease-in-out;
-  @keyframes spin {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
+
+  div {
+    border: 4px solid #222831;
+    border-top: 4px solid #393e46;
+    border-radius: 50%;
+    border-style: dotted;
+    max-width: ${(props) => props.size || '25px'};
+    max-height: ${(props) => props.size || '25px'};
+    width: 100%;
+    height: 100%;
+
+    animation: spin 2s linear infinite;
+    transition: all 0.2s ease-in-out;
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
     }
   }
-`;
+`
 
-export const Loading = ({ maxHeight, maxWidth }) => {
+export const Loading = ({ size }) => {
   return (
-    <Container maxHeight={maxHeight} maxWidth={maxWidth}>
-      <Spinner maxHeight={maxHeight} maxWidth={maxWidth} />
-    </Container>
-  );
-};
+    <Spinner size={size}>
+      <div />
+    </Spinner>
+  )
+}
